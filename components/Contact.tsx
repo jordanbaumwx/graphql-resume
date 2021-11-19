fb7ca31ce825e39c4abc98b17db20c0480bf1007
Jordan Baumgardner <developer@jordanbaumgardner.com>
2021-11-18 18:47:13 -0700

    Document components.

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,9 +1,21 @@
+/**
+ * This component holds links to contact me.
+ * 
+ * @author Jordan Baumgardner
+ * @history 2021-11-18 Jordan Baumgardner - Original
+ */
+
+// Import icons used for the hyperlinks.
 import { FaGithub } from 'react-icons/fa';
 import { GrMail } from 'react-icons/gr';
 import {MdWebAsset} from 'react-icons/md';
 
+
+/*
+* @param {Object} props - The props of this component are immediately stripped for 
+* only the bio information.
+*/
 export default function Contact({bio}) {
-      
     return (
         <div className="grid grid-cols-2 items-center">
              <h2 className="text-gray-700 font-bold text-2xl col-span-1 content-start"> Contact </h2>
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,6 +1,18 @@
+/**
+ * This component displays information for a generic experience.
+ * 
+ * @author Jordan Baumgardner
+ * @history 2021-11-18 Jordan Baumgardner - Original
+ */
+
+// This package formats the date so it looks nice.
 import {format} from "date-fns"
 
 export default function Experience({position}) {
+    /* 
+    This formats how long I was at the company in an easy to read 
+    by humans format. 
+    */
     const length = [
         position.years > 0 ? `${position.years} years` : null,
         position.months > 0 ? `${position.months} months` : null,
@@ -8,15 +20,18 @@ export default function Experience({position}) {
       
     return (
         <div className='pt-4 px-6'>
+            {/* Display company information and my position title. */}
             <div className="grid grid-cols-1">
                 <h3 className="col-span-1 text-gray-900 font-medium text-xl align-left">{position.title}</h3>
                 <p className="col-span-2 text-gray-700 font-light text-xl align-right">{position.company} | {position.location}</p>
             </div>
+            {/* Display information about the time I was with the company. */}
             <p className="text-gray-700 font-light">
                 {format(new Date(position.startDate), "MMM yyyy")} - {position.endDate ? format(new Date(position.endDate), "MMM yyyy") : "Current"}
                 {" | "}
                 {length}
             </p>
+            {/* Creates a list of accomplishments while I was at this position.*/}
             <ul className="list-disc px-8">
                 {position.achievements.map(achievement => 
                     <li key={achievement}>{achievement}</li>
