849ba43ee453dd87d87c41f9dfa91debe67dedc1
Jordan Baumgardner <developer@jordanbaumgardner.com>
2021-11-18 18:47:53 -0700

    Document _app.tsx (setup for website)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
+/**
+ * This is the initial file next.js looks for and
+ * sets up common functionality for all pages.
+ * 
+ * @author Jordan Baumgardner
+ * @history 2021-11-18 Jordan Baumgardner - Original
+ */
+
+// Import global styling.
 import "../styles/globals.css";
 import 'tailwindcss/tailwind.css';
 
+// Import Apollo to interact with GraphQL.
 import { ApolloProvider } from "@apollo/client";
+
+// Custom code to setup connection to GraphQL.
 import { useApollo } from "../src/apolloClient";
 
 function MyApp({ Component, pageProps }) {
