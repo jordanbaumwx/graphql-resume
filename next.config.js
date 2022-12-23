1971f354ca66be0660f6678ae5974ad3f9d01c16
Jordan Baumgardner <jordanbaumgardner@farmersrisk.ag>
2022-12-23 14:25:09 -0600

    Update next.js  dependencies to get working.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 module.exports = {
+  experimental: { appDir: true },
   reactStrictMode: true,
   webpack: (config, options) => {
     config.experiments = {
+      ...config.experiments,
       topLevelAwait: true,
+      layers: true,
     };
+
     return config;
   },
 };
